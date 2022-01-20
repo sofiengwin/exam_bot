f9af8c063fd002529e919b4fd86737ec79d0983d
sofiengwin <godwin.onisofien@andela.com>
2022-01-19 20:33:51 -0800

    return response from lambda

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import {sendAPi} from './util/sendApi'
 console.log("djdjdjdjjd", process.env)
 exports.handler = async () => {
   console.log('before api')
-  await sendAPi()
+  const result = await sendAPi()
   console.log('after api')
 
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(result),
+  }
 }
